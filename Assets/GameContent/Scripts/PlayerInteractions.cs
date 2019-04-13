407a059550a9f029bce8181bf3f03dfaf456f38e
Kerian <kerian.simon@e-artsup.net>
2019-04-13 19:12:08 +0200

    gitignore update + scripts player interactions

---
+++ Assets/GameContent/Scripts/PlayerInteractions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, LayerMask.GetMask("Interactive")))
+        {
+            Debug.Log(hit);
+        }
+
+    }
+} //Fin du script --> Pierrick + Kérian
