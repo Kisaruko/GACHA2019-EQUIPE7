d75ef840a266be8a1eefba230cb82e48bd4431b4
Julien <julien.delaunay034@gmail.com>
2019-04-15 13:45:58 +0200

    Système de dissolve progressif

--- Assets/GameContent/Scripts/Effects/DissolveControler.cs
+++ Assets/GameContent/Scripts/Effects/DissolveControler.cs
@@ -7,13 +7,25 @@ public class DissolveControler : MonoBehaviour
     private Material _mat;
 
     [SerializeField, Range(0, 1)] private float _dissolveAmount;
+    public bool general;
+    private float targetAmount;
+    public float speed = 0.5f;
 
 
     public void SetDissolveAmount(float amount)
     {
-        _dissolveAmount = amount;
+        if (_mat == null) return;
+
+        targetAmount = amount;
+    }
+
+
+    private void Update()
+    {
+        _dissolveAmount = Mathf.Lerp(_dissolveAmount, targetAmount, Time.fixedDeltaTime / speed);
+
         if (_mat != null)
-        {
+        {         
             _mat.SetFloat("_DissolveAmount", _dissolveAmount);
         }
     }
