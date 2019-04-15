b16c957c72d74b3112613493c5919d634165b5bd
Lucas <lucasjoestar@outlook.fr>
2019-04-15 10:16:52 +0200

    New cadenas

--- Assets/GameContent/Scripts/GPE/ObjectFocus.cs
+++ Assets/GameContent/Scripts/GPE/ObjectFocus.cs
@@ -8,11 +8,22 @@ public class ObjectFocus : MonoBehaviour
     private Camera _focusCamera;
     private Collider _interactionCollider;
 
+    private bool _focusing;
+
 
     private void Awake()
     {
         _focusCamera = GetComponentInChildren<Camera>();
         _interactionCollider = GetComponent<Collider>();
+    }
+
+
+    private void Update()
+    {
+        if(_focusing && Input.GetMouseButtonDown(1))
+        {
+            LeaveFocusView(FindObjectOfType<PlayerInteractions>());
+        }
     }
 
 
@@ -25,6 +36,7 @@ public class ObjectFocus : MonoBehaviour
         PlayerInteractions playerInteraction = FindObjectOfType<PlayerInteractions>();
         _interactionCollider.enabled = !isFocus;
         Cursor.visible = isFocus;
+        _focusing = isFocus;
 
         if (isFocus)
         {
@@ -46,12 +58,18 @@ public class ObjectFocus : MonoBehaviour
 
         yield return new WaitForSeconds(1);
 
+        LeaveFocusView(playerInteraction);
+        gameObject.tag = "Untagged";
+    }
+
+    private void LeaveFocusView(PlayerInteractions playerInteraction)
+    {
         Cursor.lockState = CursorLockMode.Locked;
         //Reactivate player controler
         playerInteraction.Active();
+        _interactionCollider.enabled = true;
         GetComponentInChildren<EnigmeCadenaMenu>().currentCamera = playerInteraction.controller.camera;
         _focusCamera.depth = -5;
-        gameObject.tag = "Untagged";
     }
 
 }
